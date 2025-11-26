b045751f6d25a35166f738c9e00d7c22cee7bb46
Charlie <chowgue0106@g.educaand.es>
2025-11-26 12:55:01 +0100

    limpiando los arraylists

--- src/main/java/TalentShow/modelo/procesos/Datos.java
+++ src/main/java/TalentShow/modelo/procesos/Datos.java
@@ -6,26 +6,24 @@ import TalentShow.modelo.beans.Malabarista;
 import TalentShow.modelo.beans.Poeta;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Datos{
     public ArrayList<String> Nombres() {
-        ArrayList<String> nombres = new ArrayList<>();
-        nombres.add("Juan");
-        nombres.add("Pedro");
-        nombres.add("María");
-        nombres.add("José");
-        nombres.add("Josefa");
-        nombres.add("Carlota");
-        nombres.add("Mari Ángeles");
-        nombres.add("Eugenio");
-        nombres.add("Francisco José");
-        nombres.add("Carolina");
-        nombres.add("Laura");
-        nombres.add("Iván");
-        nombres.add("Ángela");
-        nombres.add("Pepe");
-        nombres.add("Charlie");
-        return nombres;
+        return new ArrayList<>(List.of("Juan", "Pedro", "María", "José", "Josefa", "Carlota", "Mari Ángeles", "Eugenio", "Francisco José", "Laura","Ángela"));
+    }
+    public ArrayList<String> Poemas() {
+        return new ArrayList<>(List.of("Mio Cid","A un olmo seco","Rima LIII","Tú me quieres alba","La canción del pirata",
+                "Oda a la cebolla","The Raven","Poema 20","La Odisea","El camino no elegido","Beowulf","Coplas a la muerte de su padre"));
+    }
+
+    public ArrayList<String> Objetos() {
+        return new ArrayList<>(List.of("pelotas","macetas","bolas","papeles higiénicos","cabezas","cebollas","hámsters","extintores"));
+    }
+
+    public ArrayList<String> Canciones() {
+        return new ArrayList<>(List.of("Like a Rolling Stone","Imagine","Claro de Luna","Superestrella","Für Elise","Despacito",
+                "Shape of You","Respect","Cumpleaños feliz","A quién le importa","No Dudaría","Fiesta pagana","Vivir así es Morir de amor"));
     }
 
     public ArrayList<Concursante> crearConcursante(int tamanio_concurso){
@@ -40,54 +38,5 @@ public class Datos{
         return concursantes;
     }
 
-    public ArrayList<String> Poemas() {
-        ArrayList<String> poemas = new ArrayList<>();
-        poemas.add("Mio Cid");
-        poemas.add("A un olmo seco");
-        poemas.add("Rima LIII");
-        poemas.add("Tú me quieres alba");
-        poemas.add("La canción del pirata");
-        poemas.add("Oda a la cebolla");
-        poemas.add("The Raven");
-        poemas.add("Poema 20");
-        poemas.add("La Odisea");
-        poemas.add("El camino no elegido");
-        poemas.add("Beowulf");
-        poemas.add("Coplas a la muerte de su padre");
 
-        return poemas;
-    }
-
-    public ArrayList<String> Objetos() {
-        ArrayList<String> objetos = new ArrayList<>();
-        objetos.add("pelotas");
-        objetos.add("macetas");
-        objetos.add("bolas");
-        objetos.add("papeles higiénicos");
-        objetos.add("cabezas");
-        objetos.add("cebollas");
-        objetos.add("hámsters");
-        objetos.add("extintores");
-
-        return objetos;
-    }
-
-    public ArrayList<String> Canciones() {
-        ArrayList<String> canciones = new ArrayList<>();
-        canciones.add("Like a Rolling Stone");
-        canciones.add("Imagine");
-        canciones.add("Claro de Luna");
-        canciones.add("Superestrella");
-        canciones.add("Für Elise");
-        canciones.add("Despacito");
-        canciones.add("Shape of You");
-        canciones.add("Respect");
-        canciones.add("Cumpleaños feliz");
-        canciones.add("A quién le importa");
-        canciones.add("No Dudaría");
-        canciones.add("Fiesta pagana");
-        canciones.add("Vivir así es Morir de amor");
-
-        return canciones;
-    }
 }
